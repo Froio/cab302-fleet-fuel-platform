34bf27d8defe89767b60462b425036d313339838
Thomas Froio <thomasfroio@yahoo.com>
2026-08-21 15:31:12 +1000

    initial commit

---
+++ src/main/java/com/fuelfleet/cab302fleetfuelplatform/Launcher.java
@@ -0,0 +1,9 @@
+package com.fuelfleet.cab302fleetfuelplatform;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
