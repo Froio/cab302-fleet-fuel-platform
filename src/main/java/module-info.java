6f9550f3f714d87c6a50e41748c539e768fa419b
angad <n12420662@qut.edu.au>
2026-09-20 08:50:21 +1000

    build: open model package for tests

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,4 +8,5 @@ module com.fuelfleet.cab302fleetfuelplatform {
     opens com.fuelfleet.cab302fleetfuelplatform to javafx.fxml;
     exports com.fuelfleet.cab302fleetfuelplatform;
     exports com.fuelfleet.cab302fleetfuelplatform.model;
+    opens com.fuelfleet.cab302fleetfuelplatform.model;
 }
